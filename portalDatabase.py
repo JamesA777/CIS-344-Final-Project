b8e4603ed475c00d424d3aae0b19b0c93e079c1c
JamesA777 <116034093+JamesA777@users.noreply.github.com>
2023-12-18 23:45:52 -0500

    Add files via upload

---
+++ portalDatabase.py
@@ -0,0 +1,93 @@
+import mysql.connector
+from mysql.connector import Error
+
+class Database():
+    def __init__(self,
+                 host="localhost",
+                 port="3306",
+                 database="hospital_portal",
+                 user='root',
+                 password='Steven1200@'):
+
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+        self.connection = None
+        self.cursor = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                port=self.port,
+                database=self.database,
+                user=self.user,
+                password=self.password)
+            
+            if self.connection.is_connected():
+                return
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+    def addPatient(self, patient_name, age, admission_date, discharge_date):
+        ''' Method to insert a new patient into the patients table '''
+        if self.connection.is_connected():
+            self.cursor = self.connection.cursor()
+            query = "INSERT INTO patients (patient_name, age, admission_date, discharge_date) VALUES (%s, %s, %s, %s)"
+            self.cursor.execute(query, (patient_name, age, admission_date, discharge_date))
+            self.connection.commit()
+            return
+    def addDoctor(self, doc_id, d_name, email, phone, specialization):
+        ''' Method to insert a new doctor into the doctor table '''
+        if self.connection.is_connected():
+            self.cursor = self.connection.cursor()
+            query = "INSERT INTO doctors (doc_id, d_name, email, phone, specialization) VALUES (%s, %s, %s, %s, %s)"
+            self.cursor.execute(query, (doc_id, d_name, email, phone, specialization))
+            self.connection.commit()
+            return
+
+    def getAllPatients(self):
+        ''' Method to get all patients from the patients table '''
+        if self.connection.is_connected():
+            self.cursor = self.connection.cursor()
+            query = "SELECT * FROM patients"
+            self.cursor.execute(query)
+            records = self.cursor.fetchall()
+            return records
+
+    def scheduleAppointment(self, patient_id, doctor_id, appointment_date, appointment_time):
+        ''' Method to schedule an appointment '''
+        # Implement the functionality
+        if self.connection.is_connected():
+            self.cursor = self.connection.cursor()
+            query = "INSERT INTO Appointments (patient_id, doctor_id, appointment_date, appointment_time) VALUES (%s, %s, %s, %s)"
+            self.cursor.execute(query, (patient_id, doctor_id, appointment_date, appointment_time))
+            self.connection.commit()
+            return
+        pass
+
+    def viewAppointments(self):
+        ''' Method to view all appointments '''
+        # Implement the functionality
+        if self.connection.is_connected():
+            self.cursor = self.connection.cursor()
+            query = "SELECT * FROM Appointments"
+            self.cursor.execute(query)
+            records = self.cursor.fetchall()
+            return records
+
+    def dischargePatient(self, id):
+        ''' Method to discharge a patient '''
+        # Implement the functionality
+        if self.connection.is_connected():
+             self.cursor = self.connection.cursor()
+             query = "UPDATE patients SET discharge_date = current_date WHERE patient_id = id"
+             self.cursor.execute(query, (id))
+             self.connection.commit()
+        pass
+
+    # Add more methods as needed for hospital operations
+
